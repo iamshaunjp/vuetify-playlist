0e01cadf3b272c9f8560ccdad469cc1898bda8bd
Shaun <shaunjp@shaun-pelling.com>
2018-10-30 19:34:33 +0000

    lesson-7

--- todo-ninja/src/components/Navbar.vue
+++ todo-ninja/src/components/Navbar.vue
@@ -1,15 +1,19 @@
 <template>
-  <v-toolbar flat app>
-    <v-toolbar-title class="text-uppercase">
-      <span class="font-weight-light">Todo</span>
-      <span>Ninja</span>
-    </v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-btn flat color="grey">
-      <span>Sign Out</span>
-      <v-icon right>exit_to_app</v-icon>
-    </v-btn>
-  </v-toolbar>
+  <nav>
+
+    <v-toolbar flat app>
+      <v-toolbar-title class="text-uppercase">
+        <span class="font-weight-light">Todo</span>
+        <span>Ninja</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat color="grey">
+        <span>Sign Out</span>
+        <v-icon right>exit_to_app</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+  </nav>
 </template>
 
 <script>
