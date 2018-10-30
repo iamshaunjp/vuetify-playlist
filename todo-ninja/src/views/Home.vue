3cdf7ab68afb11d24a0063d159155aacf75ab005
Shaun <shaunjp@shaun-pelling.com>
2018-10-30 14:54:45 +0000

    lesson-4

--- todo-ninja/src/views/Home.vue
+++ todo-ninja/src/views/Home.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="home">
     <h1>This is the homepage</h1>
+    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ipsam cupiditate in et libero blanditiis nam fuga beatae eum impedit officia temporibus voluptate error at aliquam, nobis architecto sunt atque?</p>
+    <p class="pink red--text lighten-3 text--darken-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam fugit rerum, magni beatae sapiente dolores consectetur ea laboriosam totam, nulla nemo atque veritatis quibusdam esse aliquid quaerat laborum id nihil!</p>
+    <h1 class="display-4">Massive display text</h1> 
+    <h4 class="display-1">Smaller display text</h4>
+    <p class="headline">this is a headline</p>
+    <p class="subheading">this is a sub-heading</p>
+    <p class="caption">this is a caption</p>
   </div>
 </template>
