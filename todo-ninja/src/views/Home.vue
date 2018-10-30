033183a17ca463a63906655779fce5101b5f87af
Shaun <shaunjp@shaun-pelling.com>
2018-10-30 15:52:28 +0000

    lesson-6

--- todo-ninja/src/views/Home.vue
+++ todo-ninja/src/views/Home.vue
@@ -3,28 +3,11 @@
     <h1>This is the homepage</h1>
     <v-divider></v-divider>
 
-    <v-btn class="pink white--text">click me</v-btn>
-    <v-btn depressed color="pink">click me</v-btn>
-    <v-btn flat color="pink">click me</v-btn>
+    <!-- <v-btn class="hidden-md-and-down">click me</v-btn> -->
 
-    <v-btn depressed class="pink white--text">
-      <v-icon left>email</v-icon>
-      <span>email me</span>
-    </v-btn>
+    <!-- <v-btn class="hidden-md-and-up">click me</v-btn> -->
 
-    <v-btn depressed small class="pink white--text">
-      <v-icon left small>email</v-icon>
-      <span>email me</span>
-    </v-btn>
-
-    <v-btn depressed large class="pink white--text">
-      <span>email me</span>
-      <v-icon right large>email</v-icon>
-    </v-btn>
-
-    <v-btn fab dark small depressed color="purple">
-      <v-icon dark>favorite</v-icon>
-    </v-btn>
+    <v-btn class="hidden-sm-only">click me</v-btn>
 
   </div>
 </template>
