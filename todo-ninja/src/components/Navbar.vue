85ddec92d0d4d649317b0940134ed47f5b440dc6
Shaun <shaunjp@shaun-pelling.com>
2018-10-30 19:40:05 +0000

    lesson-7

--- todo-ninja/src/components/Navbar.vue
+++ todo-ninja/src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav>
 
     <v-toolbar flat app>
-      <v-toolbar-title class="text-uppercase">
+      <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Todo</span>
         <span>Ninja</span>
       </v-toolbar-title>
