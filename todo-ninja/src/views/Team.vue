b95990f26102ed7310378dc99451bbc622f6686c
Shaun <shaunjp@shaun-pelling.com>
2018-10-31 15:48:23 +0000

    lesson-19

--- todo-ninja/src/views/Team.vue
+++ todo-ninja/src/views/Team.vue
@@ -3,8 +3,46 @@
     <h1 class="subheading grey--text">Team</h1>
 
     <v-container class="my-5">
-      content
+
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+          <v-card flat class="text-xs-center ma-3">
+            <v-card-media class="pt-4">
+              image here
+            </v-card-media>
+            <v-card-text>
+              <div class="subheading">{{ person.name }}</div>
+              <div class="grey--text">{{ person.role }}</div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn flat color="grey">
+                <v-icon small left>message</v-icon>
+                <span class="">Message</span>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+
     </v-container>
     
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      team: [
+        { name: 'The Net Ninja', role: 'Web developer' },
+        { name: 'Ryu', role: 'Graphic designer' },
+        { name: 'Chun Li', role: 'Web developer' },
+        { name: 'Gouken', role: 'Social media maverick' },
+        { name: 'Bill Gates', role: 'Director' },
+        { name: 'Yoshi', role: 'Sales guru'}
+
+      ]
+    }
+  }
+}
+</script>
