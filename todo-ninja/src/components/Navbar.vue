dbb2bd70b4ac04f33c4746629fd104fcbe537599
Shaun <shaunjp@shaun-pelling.com>
2018-10-31 11:19:49 +0000

    lesson-10

--- todo-ninja/src/components/Navbar.vue
+++ todo-ninja/src/components/Navbar.vue
@@ -15,7 +15,16 @@
     </v-toolbar>
 
     <v-navigation-drawer app v-model="drawer" class="primary">
-      <p>test</p>
+      <v-list>
+        <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-tile-action>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title class="white--text">{{ link.text }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
     </v-navigation-drawer>
 
   </nav>
@@ -25,7 +34,12 @@
 export default {
   data() {
     return {
-      drawer: false
+      drawer: false,
+      links: [
+        { icon: 'dashboard', text: 'Dashboard', route: '/' },
+        { icon: 'folder', text: 'My Projects', route: '/projects' },
+        { icon: 'person', text: 'Team', route: '/team' },
+      ]
     }
   }
 }
