e67e62070758bb716cb27fbdf5cf3f92dcacac05
Shaun <shaunjp@shaun-pelling.com>
2018-10-31 12:14:22 +0000

    lesson-13

--- todo-ninja/src/views/Dashboard.vue
+++ todo-ninja/src/views/Dashboard.vue
@@ -3,7 +3,31 @@
     <h1 class="subheading grey--text">Dashboard</h1>
 
     <v-container fluid class="my-5">
-      content
+      
+      <v-layout row wrap>
+        <v-flex xs12 md6>
+          <v-btn outline block class="primary">1</v-btn>
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outline block class="primary">2</v-btn>
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outline block class="primary">3</v-btn>
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outline block class="primary">4</v-btn>
+        </v-flex>
+      </v-layout>
+
+      <v-layout row wrap justify-space-around>
+        <v-flex xs4 md3>
+          <v-btn outline block class="success">1</v-btn>
+        </v-flex>
+        <v-flex xs4 md3>
+          <v-btn outline block class="success">2</v-btn>
+        </v-flex>
+      </v-layout>
+
     </v-container>
    
   </div>
