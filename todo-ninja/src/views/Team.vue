a15b114eb192e74066c3adff6f936ee512386fc3
Shaun <shaunjp@shaun-pelling.com>
2018-10-31 16:22:55 +0000

    lesson-20

--- todo-ninja/src/views/Team.vue
+++ todo-ninja/src/views/Team.vue
@@ -7,9 +7,11 @@
       <v-layout row wrap>
         <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
           <v-card flat class="text-xs-center ma-3">
-            <v-card-media class="pt-4">
-              image here
-            </v-card-media>
+            <v-responsive class="pt-4">
+              <v-avatar size="100" class="grey lighten-2">
+                <img :src="person.avatar">
+              </v-avatar>
+            </v-responsive>
             <v-card-text>
               <div class="subheading">{{ person.name }}</div>
               <div class="grey--text">{{ person.role }}</div>
@@ -34,12 +36,11 @@ export default {
   data() {
     return {
       team: [
-        { name: 'The Net Ninja', role: 'Web developer' },
-        { name: 'Ryu', role: 'Graphic designer' },
-        { name: 'Chun Li', role: 'Web developer' },
-        { name: 'Gouken', role: 'Social media maverick' },
-        { name: 'Bill Gates', role: 'Director' },
-        { name: 'Yoshi', role: 'Sales guru'}
+        { name: 'The Net Ninja', role: 'Web developer', avatar: '/avatar-1.png' },
+        { name: 'Ryu', role: 'Graphic designer', avatar: '/avatar-2.png' },
+        { name: 'Chun Li', role: 'Web developer', avatar: '/avatar-3.png' },
+        { name: 'Gouken', role: 'Social media maverick', avatar: '/avatar-4.png' },
+        { name: 'Yoshi', role: 'Sales guru', avatar: '/avatar-5.png'}
 
       ]
     }
