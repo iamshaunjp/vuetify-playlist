2059903541edab456652a5ec25457ea0d9b81261
Shaun <shaunjp@shaun-pelling.com>
2018-10-31 13:56:59 +0000

    lesson-15

--- todo-ninja/src/views/Dashboard.vue
+++ todo-ninja/src/views/Dashboard.vue
@@ -2,30 +2,61 @@
   <div class="dashboard">
     <h1 class="subheading grey--text">Dashboard</h1>
 
-    <v-container fluid class="my-5">
+    <v-container class="my-5">
       
-      <v-card flat>
-        <v-layout row wrap class="pa-3">
+      <v-card flat v-for="project in projects" :key="project.title">
+        <v-layout row wrap :class="`pa-3 project ${project.status}`">
           <v-flex xs12 md6>
             <div class="caption grey--text">Project title</div>
-            <div>Create a new website</div>
+            <div>{{ project.title }}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
             <div class="caption grey--text">Person</div>
-            <div>The Net Ninja</div>
+            <div>{{ project.person }}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
             <div class="caption grey--text">Due by</div>
-            <div>1st Jan 2019</div>
+            <div>{{ project.due }}</div>
           </v-flex>
           <v-flex xs2 sm4 md2>
             <div class="caption grey--text">Status</div>
-            <div>ongoing</div>
+            <div>{{ project.status }}</div>
           </v-flex>
         </v-layout>
+        <v-divider></v-divider>
       </v-card>
 
     </v-container>
    
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      projects: [
+        { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Create a phone that doesn\'t suck', person: 'Bill Gates', due: '10th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Create a community forum', person: 'Gouken', due: '20th Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+.project.complete{
+  border-left: 4px solid #3CD1C2;
+}
+.project.ongoing{
+  border-left: 4px solid orange
+}
+.project.overdue{
+  border-left: 4px solid tomato;
+}
+
+</style>
