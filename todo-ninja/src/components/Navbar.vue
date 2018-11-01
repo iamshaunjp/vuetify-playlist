323f254cb3714f188940518df7ae5150fe5b0473
Shaun <shaunjp@shaun-pelling.com>
2018-11-01 10:29:58 +0000

    lesson-22

--- todo-ninja/src/components/Navbar.vue
+++ todo-ninja/src/components/Navbar.vue
@@ -8,6 +8,20 @@
         <span>Ninja</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+
+      <!-- menu -->
+      <v-menu offset-y>
+        <v-btn flat slot="activator" color="grey">
+          <v-icon left>expand_more</v-icon>
+          <span>Menu</span>
+        </v-btn>
+        <v-list>
+          <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+            <v-list-tile-title>{{ link.text }}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+
       <v-btn flat color="grey">
         <span>Sign Out</span>
         <v-icon right>exit_to_app</v-icon>
