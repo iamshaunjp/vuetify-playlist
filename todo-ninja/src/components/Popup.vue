4a46c90e8395b44d501fe079c34d6c7342eaae68
Shaun <shaunjp@shaun-pelling.com>
2018-11-01 11:42:08 +0000

    lesson-25

--- todo-ninja/src/components/Popup.vue
+++ todo-ninja/src/components/Popup.vue
@@ -7,9 +7,18 @@
       </v-card-title>
       <v-card-text>
         <v-form class="px-3">
-          <v-text-field v-model="title" label="Title"></v-text-field>
-          <v-textarea v-model="content" label="Information"></v-textarea>
+          <v-text-field v-model="title" label="Title" prepend-icon="folder"></v-text-field>
+          <v-textarea v-model="content" label="Information" prepend-icon="edit"></v-textarea>
+
+          <v-menu v-model="menu" :close-on-content-click="false">
+            <v-text-field slot="activator" 
+              :value="formattedDate" clearable label="Due date" prepend-icon="date_range">
+            </v-text-field>
+            <v-date-picker v-model="due" @change="menu = false"></v-date-picker>
+          </v-menu>
+
           <v-spacer></v-spacer>
+
           <v-btn flat @click="submit" class="success mx-0 mt-3">Add Project</v-btn>
         </v-form>
       </v-card-text>
@@ -18,16 +27,26 @@
 </template>
 
 <script>
+import format from 'date-fns/format'
+
 export default {
   data() {
     return {
       title: '',
-      content: ''
+      content: '',
+      due: null,
+      menu: false
     }
   },
   methods: {
     submit() {
       console.log(this.title, this.content)
+    }
+  },
+  computed: {
+    formattedDate () {
+      console.log(this.due)
+      return this.due ? format(this.due, 'Do MMM YYYY') : ''
     }
   }
 }
