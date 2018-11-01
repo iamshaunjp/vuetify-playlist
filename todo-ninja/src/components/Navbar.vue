119b25b00646a206f062c30a0b90e9503013c8c2
Shaun <shaunjp@shaun-pelling.com>
2018-11-01 10:47:38 +0000

    lesson-21

--- todo-ninja/src/components/Navbar.vue
+++ todo-ninja/src/components/Navbar.vue
@@ -15,6 +15,14 @@
     </v-toolbar>
 
     <v-navigation-drawer app v-model="drawer" class="primary">
+      <v-layout column align-center>
+        <v-flex class="mt-5">
+          <v-avatar size="100">
+            <img class="text-lg-center" src="/avatar-1.png">
+          </v-avatar>
+          <p class="white--text subheading mt-1">The Net Ninja</p>
+        </v-flex>
+      </v-layout>
       <v-list>
         <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
           <v-list-tile-action>
