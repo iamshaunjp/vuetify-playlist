d8c6124beb0e9556592984b961883d2541431f78
Shaun <shaunjp@shaun-pelling.com>
2018-11-01 10:43:24 +0000

    lesson-23

---
+++ todo-ninja/src/components/Popup.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-layout>
+    <v-dialog max-width="600px">
+      <v-btn flat slot="activator" class="success">Add New Project</v-btn>
+      <v-card>
+        <v-card-title>
+          <h2>Add a New Project</h2>
+        </v-card-title>
+      </v-card>
+    </v-dialog>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
